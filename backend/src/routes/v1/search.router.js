c077e72dbbebc0ba970c78b0c21735d8ab35f2dd
Mohammed Aladl <aladlmuhammed97@gmail.com>
2025-09-15 15:28:19 +0300

    feat: add search controllers and endpoints

---
+++ backend/src/routes/v1/search.router.js
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router();
+
+import { validateToken } from "../../middlewares/index.js";
+
+import * as searchConrtoller from "../../controllers/search/index.js";
+
+router.use(validateToken);
+
+router.get("/", searchConrtoller.searchStocks);
+
+export default router;
