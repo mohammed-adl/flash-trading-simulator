8cbfc806054c04efc97de1e72f1defb81dd41b89
Mohammed Aladl <aladlmuhammed97@gmail.com>
2025-09-16 10:51:39 +0300

    feat: add protected layout

---
+++ frontend/src/app/(protected)/layout.js
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useParams } from "next/navigation";
+
+import { LoadingScreen } from "@/components/ui";
+import { useUser } from "@/contexts";
+import { authService } from "@/services";
+import { initSocketConnection, disconnectSocket } from "@/socket";
+
+export default function ProtectedRoute({ children }) {
+  const [loading, setLoading] = useState(true);
+  const { user } = useUser();
+  const router = useRouter();
+  const { username } = useParams();
+
+  const validateToken = async () => {
+    try {
+      const isExpired = await authService.validateAccessToken();
+      if (isExpired) {
+        const body = await authService.callRefreshToken();
+        if (body) authService.setToken(body.token);
+      }
+
+      if (user?.username && user.username !== username) {
+        router.replace(`/${user.username}`);
+        return;
+      }
+
+      initSocketConnection(user);
+    } catch (err) {
+      authService.logout();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    validateToken();
+    return () => disconnectSocket();
+  }, [user, username]);
+
+  if (loading) return <LoadingScreen />;
+  if (!user) return <LoadingScreen />;
+
+  return <>{children}</>;
+}
