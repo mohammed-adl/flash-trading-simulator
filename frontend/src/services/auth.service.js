ffada7c64de55375f808d7efd0b3cc6a7ca64bc8
Mohammed Aladl <aladlmuhammed97@gmail.com>
2025-09-16 09:41:01 +0300

    feat: add auth service

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,53 @@
+import { jwtDecode } from "jwt-decode";
+import { socket } from "@/lib";
+
+import { handleRefreshToken } from "../fetchers";
+
+const authService = {
+  setToken(token) {
+    localStorage.setItem("token", token);
+    return token;
+  },
+
+  async validateAccessToken() {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      this.logout();
+      return;
+    }
+
+    try {
+      const decoded = jwtDecode(token);
+      const currentTime = new Date().getTime() / 1000;
+      if (decoded.exp < currentTime) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      this.logout();
+    }
+  },
+
+  async callRefreshToken() {
+    try {
+      const data = await handleRefreshToken();
+      return data;
+    } catch (err) {
+      this.logout();
+    }
+  },
+
+  clearSession() {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    socket.disconnect();
+  },
+
+  logout() {
+    this.clearSession();
+    window.location.href = "/";
+  },
+};
+
+export default authService;
